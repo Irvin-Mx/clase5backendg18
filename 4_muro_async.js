f17b4c5111d7125dd6fdd3d0c8add2e28b089c45
Irvin Martinez <irvin0329@outlook.com>
2022-05-25 19:34:03 -0500

    added promises and async await js files

---
+++ 4_muro_async.js
@@ -0,0 +1,73 @@
+function construir(muro){
+    console.log('Construyendo..')
+
+    return new Promise( (resolve,reject) =>{
+        setTimeout( () => {
+            muro.construido = true;
+            resolve(muro);
+        },500)
+    })
+}
+
+function aplanar(muro){
+    console.log('Aplanando...')
+
+    return new Promise(  (resolve,reject) => {
+        setTimeout( ()=> {
+            muro.aplanado = true;
+            resolve(muro)
+        },400)
+    })
+}
+
+function pintar(muro){
+    console.log('Pintando..')
+
+    return new Promise((resolve,reject) => {
+        setTimeout( ()=> {
+            muro.pintado = true;
+            resolve(muro)
+        },300)
+    })
+}
+
+const muro = {
+    construido:false,
+    aplanado:false,
+    pintado:false,
+}
+
+//!Promises
+
+// const promesaConstruir = construir(muro);
+// promesaConstruir
+//     .then( (muroConstruido)=>{
+//         console.log('Muro actualmente', muroConstruido);
+//         return aplanar(muroConstruido)  //Esta promesa se pasa al then siguiente
+//     })
+//     .then( (muroAplanado) =>{
+//         console.log('Muro actualmente:', muroAplanado)
+
+//         return pintar(muroAplanado)
+//     })
+//     .then( (muroPintado) =>{
+//         console.log('Muero en terer then:', muroPintado)
+
+//         console.log('Muro terminado')
+
+//     })
+
+
+
+
+
+//!Async await
+async function ordenarConstruccion(){
+    /*const muroConstruido = */ await construir(muro);
+    /*const muroAplanado = */ await aplanar(muroConstruido);
+    /*const muroPintado = */ await pintar(muroAplanado);
+
+    console.log('se termino el muro', muroPintado);
+}
+
+ordenarConstruccion();
